7ef713c257efcc1b284f47cb139912848ca3f1c5
Edilson Rivero <82335981+Erivck@users.noreply.github.com>
2023-11-01 21:09:13 -0400

    Delete dist directory

--- dist/utils/isAnEmptyObject/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isAnEmptyObject = void 0;
-const isAnEmptyObject = (object) => {
-    return !Object.values(object).filter(Boolean).length;
-};
-exports.isAnEmptyObject = isAnEmptyObject;
-//# sourceMappingURL=index.js.map
--- dist/utils/omitPropertyFromObject/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.omitPropertyFromObject = void 0;
-const omitPropertyFromObject = (object, property) => {
-    const { [property]: _, ...filteredObject } = object;
-    return filteredObject;
-};
-exports.omitPropertyFromObject = omitPropertyFromObject;
-//# sourceMappingURL=index.js.map
